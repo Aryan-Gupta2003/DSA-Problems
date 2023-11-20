132d733caf7e04d575899a5afefd5b28cf2db1fa
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2023-11-20 19:05:52 +0530

    Best Time to Buy and Sell Stock

---
+++ Easy/buy_and_sell.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxprofit(int arr[], int n)
+{
+    int cp = arr[0], p = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < cp)
+        {
+            cp = arr[i];
+        }
+        else if ((arr[i] - cp) > p)
+        {
+            p = (arr[i] - cp);
+        }
+    }
+    return p;
+}
+int main()
+{
+    int a[10], n, x, c, s, e;
+    cout << "Enter no. of elements in array A: ";
+    cin >> n;
+    cout << "Enter elements in array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int profit = maxprofit(a, n);
+    cout << profit << endl;
+    return 0;
+}
