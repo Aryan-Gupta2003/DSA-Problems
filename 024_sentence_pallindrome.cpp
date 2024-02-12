b76c31a5a75349681b6e3352db7d0d86cf171182
Aryan-Gupta2003 <aryang.gzb@gmail.com>
2024-02-12 15:47:02 +0530

    Add new file

---
+++ 024_sentence_pallindrome.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool pallindrome(string str)
+{
+    int s = 0, e = str.length() - 1;
+    for (int i = 0; i < e; i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+
+    string str2 = str;
+    reverse(str2.begin(), str2.end());
+
+    return str == str2;
+}
+
+int main()
+{
+    string str = "Too hot to hoot";
+    if (pallindrome(str))
+        cout << "Sentence is in pallindrome" << endl;
+    else
+        cout << "Sentence is not in pallindrome" << endl;
+    return 0;
+}
